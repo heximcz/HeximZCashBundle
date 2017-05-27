2a7a7bf8866cf7c2e9b17a2d4acbce9615c904a2
heximcz <admin@best-net.cz>
2017-05-27 23:09:39 +0200

    listtransactions and getwalletinfo

--- Zcash/ZcashWallet.php
+++ Zcash/ZcashWallet.php
@@ -6,8 +6,119 @@
 
 namespace Hexim\HeximZcashBundle\Zcash;
 
+use Hexim\HeximZcashBundle\Zcash\ZcashWrapper;
 
-class ZcashWallet
+
+class ZcashWallet implements ZcashWalletInterface
 {
+    /**
+     * @var ZcashWrapper $wrapper
+     */
+    private $wrapper;
 
+    /**
+     * Default command tail
+     * @var array $defaultCommand
+     */
+    private $defaultCommand = [
+        "jsonrpc" => "1.0",
+        "id" => "curl"
+    ];
+
+    private $result;
+
+    /**
+     * ZcashWallet constructor.
+     * @param array $params
+     */
+    public function __construct($params)
+    {
+        $this->wrapper = new ZcashWrapper($params);
+    }
+
+    /**
+     * @return array
+     */
+    public function getWalletInfo()
+    {
+        return $this->wrapper->rpcZcashCommand(
+            $this->mergeCommand([
+                'method' => 'getwalletinfo',
+                'params' => []
+            ])
+        );
+    }
+
+    /**
+     * The total number of transactions in the wallet
+     * @return int
+     */
+    public function getTxCount()
+    {
+        $data = $this->getWalletInfo();
+        return $data['result']['txcount'];
+    }
+
+    /**
+     * @param int $count
+     * @param int $from
+     * @param bool $includeWatchOnly
+     * @return array
+     */
+    public function listTransactions($count = 10, $from = 0, $includeWatchOnly = false)
+    {
+        $this->result = $this->wrapper->rpcZcashCommand(
+            $this->mergeCommand([
+                'method' => 'listtransactions',
+                'params' => ["*",$count,$from,$includeWatchOnly]
+            ])
+        );
+
+        if ($this->checkResponse())
+            $this->fixScientificNumbers();
+        return $this->result;
+    }
+
+    public function getNewAddress()
+    {
+        // TODO: Implement getNewAddress() method.
+    }
+
+    public function z_getNewAddress()
+    {
+        // TODO: Implement z_getNewAddress() method.
+    }
+
+    /* private functions */
+
+    private function mergeCommand($array)
+    {
+        return array_merge($this->defaultCommand, $array);
+    }
+
+    private function checkResponse()
+    {
+        if (is_null($this->result['error']) && $this->result['result']!='')
+            return true;
+        throw new \Exception("Result error!");
+    }
+
+    /**
+     * Fix: example -6.7E-6 to -0.000067
+     */
+    private function fixScientificNumbers()
+    {
+        foreach ($this->result['result'] as $key => $value)
+        {
+            if (isset($value['amount']))
+                $this->result['result'][$key]['amount'] = $this->formatZecNumber($value['amount']);
+            if (isset($value['fee']))
+                $this->result['result'][$key]['fee'] = $this->formatZecNumber($value['fee']);
+        }
+    }
+
+    private function formatZecNumber($value)
+    {
+        return number_format($value,8);
+    }
 }
---
+++ Zcash/ZcashWrapper.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by HeximCZ
+ * Date: 5/27/17 3:07 PM
+ */
+
+namespace Hexim\HeximZcashBundle\Zcash;
+
+class ZcashWrapper
+{
+    /**
+     * @var string $rpc_address
+     */
+    private $rpc_address;
+    /**
+     * @var string $rpc_user
+     */
+    private $rpc_user;
+    /**
+     * @var string $rpc_password
+     */
+    private $rpc_password;
+    /**
+     * @var int $curl_timeout
+     */
+    private $curl_timeout = 10;
+    /**
+     * @var string $return_data
+     */
+    private $return_data;
+    /**
+     * @var int $return_status
+     */
+    private $return_status;
+
+    /**
+     * ZcashWrapper constructor.
+     * @param array $params
+     */
+    public function __construct($params)
+    {
+        $this->rpc_address = $params['rpc_address'];
+        $this->rpc_password = $params['rpc_password'];
+        $this->rpc_user = $params['rpc_user'];
+    }
+
+    /**
+     * @param array $command
+     * @return array
+     * @throws \Exception
+     */
+    public function rpcZcashCommand($command)
+    {
+        if (is_array($command))
+        {
+            $this->postCommand($command);
+            $this->checkReturnCodeStatus();
+            return json_decode($this->return_data,true, 512,JSON_BIGINT_AS_STRING);
+        }
+        throw new \Exception("RPC input command is not an array!");
+    }
+
+
+    /**
+     * @param array $command
+     */
+    private function postCommand($command)
+    {
+        $con = curl_init();
+        curl_setopt($con,CURLOPT_URL,$this->rpc_address);
+        curl_setopt($con,CURLOPT_TIMEOUT,$this->curl_timeout);
+        curl_setopt($con,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($con, CURLOPT_POST,true);
+        curl_setopt($con,CURLOPT_POSTFIELDS,json_encode($command));
+        curl_setopt($con, CURLOPT_USERPWD, $this->rpc_user.':'.$this->rpc_password);
+        $this->return_data = curl_exec($con);
+        $this->return_status = curl_getinfo($con,CURLINFO_HTTP_CODE);
+        curl_close($con);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function checkReturnCodeStatus()
+    {
+        if ($this->return_status == 200)
+            return;
+        if ($this->return_status == 401)
+            throw new \Exception("Unauthorized!");
+        if ($this->return_status == 403)
+            throw new \Exception("Forbidden!");
+        if ($this->return_status == 404)
+            throw new \Exception("Not found!");
+        if ($this->return_status == 500)
+            throw new \Exception("Internal Server Error!");
+        if ($this->return_status == 502)
+            throw new \Exception("Bad Gateway!");
+        if ($this->return_status == 503)
+            throw new \Exception("Service Unavailable!");
+        throw new \Exception("Any other error. Return code no.: " . $this->return_status);
+    }
+}
